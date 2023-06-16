0d7872707623d9caf6f4d075eab6eadf354defa1
FranciscoLozCoding <z1842358@students.niu.edu>
2023-06-16 13:53:30 -0500

    plugin retrieves measurements based on a defined data format

--- Message.py
+++ Message.py
@@ -1,5 +1,4 @@
 import logging
-import base64
 import time
 from waggle.plugin import Plugin
 from parse import parse_message_payload
@@ -9,25 +8,23 @@ def on_message_publish(client, userdata, message):
 
     log_message(message) #log message
 
-    try: #get metadata and data received in bytes
+    try: #get metadata and measurements received
         metadata = parse_message_payload(message.payload.decode("utf-8"))
-        bytes_b64 = metadata["data"].encode("utf-8")
+        measurements = metadata["object"]["measurements"]
     except:
-        logging.error("Message did not contain data.")
+        logging.error("Message did not contain measurements.")
         return
-
-    #decode data
-    bytes = base64.b64decode(bytes_b64) 
-    val = bytes.decode("utf-8")
 
     #remove dynamic metadata
     try:
         metadata = clean_message_dict(metadata)
     except:
         return
+    
+    for measurement in measurements:
 
-    with Plugin() as plugin: #publish lorawan data
-        plugin.publish("lorawan.data", val, timestamp=time.time_ns(), meta=metadata)
+        with Plugin() as plugin: #publish lorawan data
+            plugin.publish(measurement["name"], measurement["value"], timestamp=time.time_ns(), meta=metadata)
 
     return
 
@@ -43,5 +40,16 @@ def log_message(message):
         "LORAWAN Message received: " + message.payload.decode("utf-8") + " with topic " + str(message.topic)
     )
     logging.info(data) #log message received
+
+    try: #get metadata and measurements received
+        metadata = parse_message_payload(message.payload.decode("utf-8"))
+        measurements = metadata["object"]["measurements"]
+    except:
+        logging.error("Message did not contain measurements.")
+        return
+    
+    for measurement in measurements:
+
+        logging.info(str(measurement["name"]) + ": " + str(measurement["value"]))
 
     return
