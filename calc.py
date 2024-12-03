b9822fd5028f5e9a91a8a5af1e3ffdd9f1676f21
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-12-03 15:25:50 -0600

    new calculator class

---
+++ calc.py
@@ -0,0 +1,40 @@
+import time
+
+class PacketLossCalculator:
+    def __init__(self):
+        self.fCnt = 0  # Last frame count
+        self.totalpl = 0  # Total packet loss
+        self.pckcount = 0  # Total packets received
+        #TODO: change plr_rate_sec to an arg
+        self.plr_sec = 3600 # rate of plr calculation in seconds, 1 hour = 3600 seconds
+        self.last_calculation_time = time.time()  # Track the last calculation time
+
+    def process_packet(self, fCnt):
+        """Process the packet to calculate packet loss and packet loss rate based on a time interval"""
+        # Initialize fCnt if not already set
+        if fCnt and self.fCnt == 0:
+            self.fCnt = fCnt
+
+        # Calculate packet loss based on fCnt difference
+        if fCnt > self.fCnt:
+            pl = (fCnt - self.fCnt - 1)
+            self.totalpl += pl
+
+        # Update the last received fCnt
+        self.fCnt = fCnt
+
+        # Increment packet count
+        self.pckcount += 1
+
+        # Calculate packet loss rate every hour
+        current_time = time.time()
+        if current_time - self.last_calculation_time >= self.plr_sec:
+            plr = self.totalpl / (self.pckcount + self.totalpl) if (self.pckcount + self.totalpl) > 0 else 0
+            
+            # Reset for the next hour
+            self.totalpl = 0
+            self.pckcount = 0
+            self.last_calculation_time = current_time
+
+            return (pl,plr)
+        return (pl,None)
